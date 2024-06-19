06b2c505b6b809d61e386d816d43e783e56ab6e6
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-06-19 09:29:35 +0300

    PoV ui PoVViewModel

--- app/src/main/java/com/example/pov/ui/feature/pov/PoVViewModel.kt
+++ app/src/main/java/com/example/pov/ui/feature/pov/PoVViewModel.kt
@@ -1,15 +1,21 @@
 package com.example.pov.ui.feature.pov
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.data.common.result.ErrorResponse
+import com.example.data.common.result.PoVResult
 import com.example.data.data.model.pov.PoV
 import com.example.data.data.repository.pov.PoVRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 private const val TAG = "PoVViewModel"
@@ -20,12 +26,60 @@ class PoVViewModel @Inject constructor(
 ) : ViewModel() {
     /* error message */
     private val _errorMessage = MutableSharedFlow<String>()
-    val errorMessage = _errorMessage.asSharedFlow()
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(TIME_IN_MILLIS),
-            initialValue = ""
-        )
+    val errorMessage = _errorMessage.asSharedFlow().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(TIME_IN_MILLIS),
+        initialValue = ""
+    )
+
+    private val _poVUiState = MutableSharedFlow<PoVUiState>()
+    val poVUiState: StateFlow<PoVUiState> = _poVUiState.asSharedFlow().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(TIME_IN_MILLIS),
+        initialValue = PoVUiState.Loading
+    )
+
+    fun updatePoVUiState(poV:PoV){
+
+    }
+
+    init {
+        viewModelScope.launch {
+            poVRepository.getAllPoVs().map { poVResultList: PoVResult<List<PoV>> ->
+                when (poVResultList) {
+                    is PoVResult.Success -> {
+                        _poVUiState.emit(
+                            PoVUiState.Success(poVResultList.data)
+                        )
+                    }
+
+                    is PoVResult.Error -> {
+                        Log.d(
+                            TAG,
+                            "PoV getAllPoVs error: ${poVResultList.responseErrorMessage}",
+                            poVResultList.throwable
+                        )
+                        _poVUiState.emit(
+                            PoVUiState.Error(
+                                throwable = poVResultList.throwable,
+                                responseErrorMessage = poVResultList.responseErrorMessage
+                            )
+                        )
+                    }
+
+                    PoVResult.Loading -> {
+                        _poVUiState.emit(
+                            PoVUiState.Loading
+                        )
+                    }
+                }
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIME_IN_MILLIS),
+                initialValue = PoVResult.Loading
+            ).collect()
+        }
+    }
 
     companion object {
         const val TIME_IN_MILLIS = 5_000L
@@ -35,12 +89,10 @@ class PoVViewModel @Inject constructor(
 sealed interface PoVUiState {
     data object Loading : PoVUiState
     data class Success(
-        val povs: List<PoV> = emptyList(),
-        val isEditEntryValid: Boolean = false
+        val povs: List<PoV> = emptyList(), val isEditEntryValid: Boolean = false
     ) : PoVUiState
 
     data class Error(
-        val throwable: Throwable? = null,
-        val responseErrorMessage: ErrorResponse? = null
+        val throwable: Throwable? = null, val responseErrorMessage: ErrorResponse? = null
     ) : PoVUiState
 }
